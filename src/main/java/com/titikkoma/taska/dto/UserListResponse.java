8291513b63f68ba9f8316433acc0e3254a7d49df
naufal.rachmat <naufal.rachmat@advancegroup.com>
2025-06-15 19:04:54 +0700

    daffa changes

--- src/main/java/com/titikkoma/taska/dto/UserListResponse.java
+++ src/main/java/com/titikkoma/taska/dto/UserListResponse.java
@@ -4,9 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.sql.Timestamp;
-
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
